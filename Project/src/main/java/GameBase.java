be115b9bd872d0997504b332cb51c6e0f3a3756a
Yumibyte <ashraigosa@gmail.com>
2022-02-20 23:19:43 -0800

    setup gameBase

--- Project/src/main/java/GameBase.java
+++ Project/src/main/java/GameBase.java
@@ -1,6 +1,36 @@
+import java.util.ArrayList;
+
 public class GameBase {
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    public String title = "";
+    public String gameType = "";
+    public ArrayList<String> availablePlayers = new ArrayList<String>();
+    private int isSortByPlayerTypeSelected = 0;
+
+    public ArrayList<String> parseRoster(String gameType) {
+        //
+        ArrayList<String> roster = new ArrayList<String>();
+
+        return roster;
     }
+
+    public String generateTitle(String gameType) {
+        return "";
+    }
+
+    public ArrayList<String> sortAlphabetically(ArrayList<String> availablePlayers) {
+        ArrayList<String> x = new ArrayList<String>();
+
+        return x;
+    }
+
+    public ArrayList<String> sortByPlayer(ArrayList<String> availablePlayers) {
+        ArrayList<String> x = new ArrayList<String>();
+
+        return x;
+    }
+
+
+
+
 }
