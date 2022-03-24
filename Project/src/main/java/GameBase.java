f33d655f4b9aed527b7e0e76c8f2cfb1a956c4c6
Yumibyte <ashraigosa@gmail.com>
2022-03-23 19:48:22 -0700

    added local files for JAR

--- Project/src/main/java/GameBase.java
+++ Project/src/main/java/GameBase.java
@@ -1,11 +1,20 @@
+import jdk.jfr.internal.tool.Main;
 import jdk.nashorn.internal.parser.JSONParser;
+import sun.misc.ClassLoaderUtil;
 
+import javax.annotation.Resources;
 import javax.swing.*;
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GameBase {
 
@@ -26,18 +35,17 @@ public class GameBase {
 
         // read roster and initialize variables
         public void readRoster() {
-//            File rosterFile = new File(filePathLocation);
-            File rosterFile = new File("src/main/java/Main Roster Database - Tracker.tsv");
+            // The class loader that loaded the class
+            try (InputStream inputStream = getClass().getResourceAsStream("/MainRosterDatabase-Tracker.tsv");
+                BufferedReader TSVReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            try (BufferedReader TSVReader = new BufferedReader(new FileReader(rosterFile))) {
+                    String line = "";
+                    while ((line = TSVReader.readLine()) != null) {
+                        String[] lineItems = line.split("\t"); //splitting the line and adding its items in String[]
+                        rosterData.add(lineItems); //adding the splitted line array to the ArrayList
+                    }
 
-                // store each row of roster onto an arraylist
-                String line = "";
-                while ((line = TSVReader.readLine()) != null) {
-                    String[] lineItems = line.split("\t"); //splitting the line and adding its items in String[]
-                    rosterData.add(lineItems); //adding the splitted line array to the ArrayList
-                }
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
